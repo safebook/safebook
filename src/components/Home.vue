902f0d613dcca10eecb27294eab74ba7a0b4dd82
Maxime Lalisse <maxime.lalisse@gmail.com>
2021-10-16 22:18:46 +0200

    vuex + router

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h3>Safebook</h3>
+    <p>
+      cryptographic social network
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">Hello world</a>.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
+
---
+++ src/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+import Home from './components/Signup.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }, {
+      path: '/hello_world',
+      name: 'hello_world',
+      component: HelloWorld
+    }, {
+      path: '/signup',
+      name: 'signup',
+      component: Signup
+    },
+  ]
+})
