33fc27af8e52d86490483c0bf065352047a29419
Maxime Lalisse <maxime.lalisse@gmail.com>
2021-10-16 22:55:00 +0200

    signup

---
+++ src/components/Signup.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="signup">
+    <p id="hello">Bonjour <input type="text" placeholder="Alice"></p>
+    <p>Voici votre addresse unique:
+    <p id="address"><b>SBhPXPB3bNHec6YvBdtRjLcFrzHzjgwiVUnWSFQmEtfMMmpM82</b></p>
+    <p>
+      Voici votre mot de passe :
+    </p>
+    <p id="password">
+      8Xii3jPyGyAwX8fDWL8uE9p7ExXyCzTJgBTVvVtKjupDrAvmF2
+    </p>
+    <p>
+      Ou vous pouvez utiliser cette phrase mnémotechnique
+    </p>
+    <p id="passphrase">
+      <span>faint</span>
+      <span>guess</span>
+      <span>captain</span>
+      <br />
+      <span>atom</span>
+      <span>bench</span>
+      <span>access</span>
+      <br />
+      <span>harbor</span>
+      <span>inherit</span>
+      <span>mother</span>
+      <br />
+      <span>warfare</span>
+      <span>dial</span>
+      <span>flame</span>
+    </p>
+    <p>
+      <br />
+      <br />
+      <router-link to="/">Téléverser mon compte</router-link>
+      <br />
+      <router-link to="/">J'ai déjà un compte</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<style scoped>
+#signup {
+  text-align: left;
+}
+#address {
+  text-align: center;
+  font-weight: bold;
+  color: green;
+}
+#hello {
+  text-align: center;
+}
+#password {
+  text-align: center;
+  font-weight: bold;
+  color: #ff5733;
+}
+#passphrase {
+  text-align: center;
+  font-weight: bold;
+  color: #ff5733;
+}
+#passphrase span {
+  display: inline-block;
+  width: 80px;
+}
+</style>
+
