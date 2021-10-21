23419473b8207187bce6073e59de952bcf8a0f5a
Maxime Lalisse <maxime.lalisse@gmail.com>
2021-10-21 23:43:45 +0200

    up

---
+++ src/components/AddressQR.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="qrcode">
+    <vue-qr :text="url" :size="130"></vue-qr>
+  </div>
+</template>
+
+<script>
+import VueQr from 'vue-qr/src/packages/vue-qr.vue'
+
+export default {
+  name: "AddressQR",
+  components: {
+    VueQr
+  },
+  props: {
+    address: String
+  },
+  computed: {
+    url() {
+      return `https://safebook.github.io/u/${this.address}`;
+    }
+  }
+}
+</script>
+
+<style>
+  #qrcode {
+    display: inline-block;
+  }
+</style>
