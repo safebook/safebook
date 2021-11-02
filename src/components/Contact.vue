0be39d713b9a1fe34725ed55795a5558e4f08b16
Maxime Lalisse <maxime.lalisse@gmail.com>
2021-11-02 18:09:05 +0100

    up

---
+++ src/components/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="contact">
+    <a @click="goToUser()">
+      {{ nameOf(address) }}
+    </a>
+  </div>
+</template>
+
+<script>
+const safebook = require('safebook')
+import config from "@/config"
+
+export default {
+  name: 'Contact',
+  props: ['address'],
+  methods: {
+    goToUser() {
+      this.$router.push(`/u/${this.address}`)
+    },
+    nameOf(address) {
+      return safebook.name(address).join(' ')
+    },
+  },
+  computed: {
+    url() {
+      return `${config.vue_url}/u/${this.address}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/messages/Publication.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="post">
+    <div id="message">
+      {{ message.content }}
+    </div>
+    <!--div id="footer">
+    </div-->
+  </div>
+</template>
+
+<script>
+import safebook from "safebook"
+
+export default {
+  name: 'Publication',
+  props: ['message'],
+  methods: {
+    goToAuthor() {
+      this.$router.push({ name: 'user', params: { address: this.message.author } })
+    },
+    goToReceiver() {
+      this.$router.push({ name: 'user', params: { address: this.message.receiver } })
+    }
+  },
+  computed: {
+    author_name()   { return safebook.name(this.message.author).join(" ")   },
+    receiver_name() { return safebook.name(this.message.receiver).join(" ") },
+    address() {
+      return this.$route.params.address
+    }
+  }
+}
+</script>
+
+<style scoped>
+#post {
+ border: 1px solid grey;
+ margin: 10px 30px 10px 30px;
+ text-align: right;
+ padding: 8px;
+}
+#message {
+  margin: 0;
+  text-align: left;
+}
+</style>
+
