1d064bd36bc5bca3320ad7dd08de0f6af9d0c953
Maxime Lalisse <maxime.lalisse@gmail.com>
2021-11-05 01:18:31 +0100

    doc and co

---
+++ src/pages/How.vue
@@ -0,0 +1,29 @@
+<template>
+<vue-markdown>
+  {{ content }}
+</vue-markdown>
+</template>
+
+<script>
+import VueMarkdown from 'vue-markdown';
+import content from 'raw-loader!@/assets/how.md'
+export default {
+  name: 'How',
+  components: { VueMarkdown },
+  data() {
+    return {
+      content
+    }
+  }
+}
+</script>
+
+<style scoped>
+.word {
+  display: inline-block !important;
+  width: 33%;
+  text-align: center;
+  font-weight: bold;
+}
+</style>
+
