498a48f00e5805a6e4b14d93f28307d162fa32dc
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-01-18 13:40:58 +0200

    up

---
+++ src/components/AddressSquared.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="addressSquared">
+    <div class="line" v-for="line in lines" :key="line">
+      {{ line }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    address: String
+  },
+  computed: {
+    lines() {
+      return [
+        this.address.substr(0, 13),
+        this.address.substr(13, 13),
+        this.address.substr(26, 13),
+        this.address.substr(39)
+      ];
+    }
+  }
+}
+</script>
+
+<style>
+#addressSquared {
+  text-align: center;
+  font-weight: bold;
+  font-family: monospace;
+  color: green;
+  display: inline-block;
+  width: 20%;
+  margin-left: 2%;
+}
+.line {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+</style>
