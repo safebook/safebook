43f6181f1f834cf0b4f08116a87eb1cc0f90ec94
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-01-30 13:42:02 +0200

    up

---
+++ src/components/NameInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="name-input">
+    <input type="text" placeholder="Votre nom" v-model="name" />
+    <button>Send</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NameInput',
+  props: ['oldName'],
+  model() {
+    return {
+      name: this.oldName
+    }
+  },
+  methods: {
+    send() {
+      this.$store.commit({
+        type: 'attr',
+        kind: 'name',
+        value: this.message
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#name-input {
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+</style>
---
+++ src/components/PM.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="post">
+    <div id="author">
+      Author
+      {{/* message.author.slice(0,12) */}}
+    </div>
+    <div id="message">
+      {{ message.content }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: ['message'],
+}
+</script>
+
+<style>
+#post {
+ border: 2px solid green;
+ border-radius: 15px;
+ margin: 10px 30px 0 30px;
+}
+#author {
+ text-align: left;
+ margin: 10px;
+ font-weight: bold;
+}
+#message {
+ margin-bottom: 10px;
+}
+</style>
