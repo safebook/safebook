1b9e320a899f211de5f3c0d4697e7a766cd0fb87
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-04-02 13:42:11 +0200

    up

--- src/router.js
+++ src/router.js
@@ -1,57 +1,65 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/pages/Home.vue'
-import Signup from '@/pages/Signup.vue'
-import Signin from '@/pages/Signin.vue'
-import User from '@/pages/User.vue'
-import Messaging from '@/pages/Messaging.vue'
+import Vue from "vue";
+import Router from "vue-router";
+//import Home from "@/pages/Home.vue";
+import Signup from "@/pages/Signup.vue";
+import Signin from "@/pages/Signin.vue";
+import User from "@/pages/User.vue";
+import Messaging from "@/pages/Messaging.vue";
 
-import What from '@/pages/What'
-import How from '@/pages/How'
-import About from '@/pages/About'
+import What from "@/pages/What";
+import How from "@/pages/How";
+import About from "@/pages/About";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
-    }, {
-      path: '/signup',
-      name: 'signup',
-      component: Signup
-    }, {
-      path: '/signin',
-      name: 'signin',
-      component: Signin
-    }, {
-      path: '/u/:address',
-      name: 'user',
-      component: User
-    }, {
-      path: '/m',
-      name: 'messaging',
-      component: Messaging
-    }, {
-      path: '/m/:address',
-      name: 'messaging',
-      component: Messaging
-    }, {
-      path: '/what',
-      name: 'what',
-      component: What
-    }, {
-      path: '/how',
-      name: 'how',
-      component: How
-    }, {
-      path: '/about',
-      name: 'about',
-      component: About
-    }
-  ]
-})
+      path: "/",
+      name: "home",
+      component: Signup,
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: Signup,
+    },
+    {
+      path: "/signin",
+      name: "signin",
+      component: Signin,
+    },
+    {
+      path: "/u/:address",
+      name: "user",
+      component: User,
+    },
+    {
+      path: "/m",
+      name: "messaging",
+      component: Messaging,
+    },
+    {
+      path: "/m/:address",
+      name: "messaging",
+      component: Messaging,
+    },
+    {
+      path: "/what",
+      name: "what",
+      component: What,
+    },
+    {
+      path: "/how",
+      name: "how",
+      component: How,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: About,
+    },
+  ],
+});
