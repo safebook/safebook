5fa122559bcb6fae6f1857e0cfe266365fafcc3b
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-04-12 13:43:17 +0200

    tweak: fix header css issue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,10 +4,12 @@
       <h3 class="font-extrabold text-3xl tracking-tight mt-6">Safebook</h3>
     </router-link>
 
-    <div class="mx-auto h-12 mt-6 bg-transparent border rounded-lg dark:border-gray-700 lg:w-1/4 focus-within:border-primary focus-within:ring focus-within:ring-primary dark:focus-within:border-primary focus-within:ring-opacity-20">
+    <div class="mx-auto h-12 mt-6 bg-transparent border rounded-lg dark:border-gray-700 lg:w-1/3 focus-within:border-primary focus-within:ring focus-within:ring-primary dark:focus-within:border-primary focus-within:ring-opacity-20">
       <form action="/search" class="flex flex-wrap justify-between md:flex-row">
         <input type="search" name="query" placeholder="Search users" required="required" class="flex-1 h-10 px-4 text-gray-700 placeholder-gray-400 bg-transparent border-none appearance-none lg:h-12 dark:text-gray-200 focus:outline-none focus:placeholder-transparent focus:ring-0 border-gray-800" />
-        <button type="submit" class="flex items-center justify-center w-full p-2 m-1 text-white transition-colors duration-300 transform rounded-lg lg:w-10 lg:h-10 lg:p-0 bg-success hover:bg-success/70 focus:outline-none focus:bg-success/70">
+        <button type="submit" class="flex items-center justify-center w-full p-2 m-1
+          text-white transition-colors duration-300 transform rounded-lg lg:w-10 lg:h-10 lg:p-0
+          bg-black hover:bg-success/70 focus:outline-none focus:bg-success/70"> <!-- bg-success -->
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z">
             </path>
