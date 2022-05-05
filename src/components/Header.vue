660b82ac4170d5b1d217e507d73017c71738caf7
Maxime Lalisse <maxime.lalisse@winddle.com>
2022-05-05 20:01:34 +0200

    up

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,25 @@
 <template>
-  <div id="header">
-    <div class="col-20" id="logo">
+  <div class="flex flex-row">
+    <div class="p-4 basis-1/4">
       <router-link to="/">
-        Safebook
+        <h3 class="font-extrabold	text-3xl text-green-700 tracking-tight">Safebook</h3>
       </router-link>
     </div>
-    <div class="col-40">
-    </div>
-    <div class="col-40" id="logout">
-      <div v-if="!account">
-        <a @click="goToHome()">Accueil</a>
-        <div class="space"></div> <div class="space"></div>
-        <a @click="signin()">Connexion</a>
-      </div>
+    <div class="p-4 basis-3/4">
       <div v-if="account">
         <a @click="goToAccount()">Ma page</a>
         <div class="space"></div>
         <a @click="goToMessaging()">Messagerie</a>
         <div class="space"></div>
         <a @click="logout()">Déconnexion</a>
+      </div>
+      <div v-else>
+        <div class="btn btn-outline btn-accent m-2" @click="signin()">
+          Connexion
+        </div>
+        <div class="btn btn-accent m-2">
+          Inscription
+        </div>
       </div>
     </div>
   </div>
@@ -44,36 +45,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-#header {
-  border-bottom: 1px solid green;
-  background-color: green;
-}
-.col-40, .col-20 {
-  display: inline-block;
-  text-align: center;
-}
-.col-40 { width: 40% }
-.col-20 { width: 20% }
-#logo a {
-  color: white;
-  font-weight: bold;
-  font-size: 1.2em;
-  cursor: pointer;
-  text-decoration: none;
-  padding: 10px;
-}
-a {
-  color: white;
-  text-decoration: none;
-}
-.space {
-  display: inline-block;
-  width: 10px;
-}
-.text-right {
-  text-align: right;
-}
-</style>
-
