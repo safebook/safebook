8957cd2092d2aaa92066475df71885e6005567ad
Maxime Lalisse <maxime.lalisse@winddle.com>
2022-05-13 15:19:54 +0200

    Tweak: Rework tree structure

--- src/components/user/index.vue
+++ src/components/user/index.vue
@@ -29,89 +29,100 @@
 </template>
 
 <script>
-import config from "@/config"
+import config from "@/config";
 
-import Avatar from "@/components/Avatar"
-import SignedMessageInput   from "@/messages/SignedMessageInput"
-import SignedMessage  from "@/messages/SignedMessage"
-import Publication  from "@/messages/Publication"
-import Contact  from "@/components/Contact"
-import Side  from "@/components/Side"
+import Avatar from "@/components/Avatar";
+import SignedMessageInput from "@/components/messages/SignedMessageInput";
+import SignedMessage from "@/components/messages/SignedMessage";
+import Publication from "@/components/messages/Publication";
+import Contact from "@/components/Contact";
+import Side from "@/components/Side";
 
 export default {
-  name: 'Signup',
+  name: "Signup",
   components: {
-    Avatar, SignedMessageInput,
-    SignedMessage, Publication,
-    Contact, Side
+    Avatar,
+    SignedMessageInput,
+    SignedMessage,
+    Publication,
+    Contact,
+    Side,
   },
   data() {
     return {
-      scope:      'published',
-      inbox:      [],
-      outbox:     [],
-      published:  [],
-      followers:  [],
-      following:  []
-    }
+      scope: "published",
+      inbox: [],
+      outbox: [],
+      published: [],
+      followers: [],
+      following: [],
+    };
   },
   computed: {
     account() {
-      return this.$store.state.account
+      return this.$store.state.account;
     },
     address() {
-      return this.$route.params.address
+      return this.$route.params.address;
     },
     myself() {
-      if (this.account)
-        return this.address == this.account.address
-      return false
+      if (this.account) return this.address == this.account.address;
+      return false;
     },
   },
   methods: {
     logout() {
-      this.$router.push('/')
+      this.$router.push("/");
     },
     goToMessaging() {
-      this.$router.push(`/m/${this.address}`)
+      this.$router.push(`/m/${this.address}`);
     },
     goToAccount() {
-      this.$router.push(`/signup`)
+      this.$router.push(`/signup`);
     },
     addContact() {
       const follow = {
         author: this.account.address,
-        receiver: this.address
-      }
+        receiver: this.address,
+      };
       fetch(`${config.url}/${this.address}/contacts`, {
-        method: 'POST', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-        body: JSON.stringify(follow)
-      }).then((res) => { console.log(res) })
-      .catch((res) => { console.log(res) })
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(follow),
+      })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((res) => {
+          console.log(res);
+        });
       this.followers.push(follow.author);
     },
     fetch() {
       fetch(`${config.api}/u/${this.address}`)
-        .then(response => response.json())
+        .then((response) => response.json())
         .then((data) => {
-          this.published = data.published
-          this.inbox = data.inbox
-          this.outbox = data.outbox
-          this.followers = data.followers
-          this.following = data.following
-        })
-    }
+          this.published = data.published;
+          this.inbox = data.inbox;
+          this.outbox = data.outbox;
+          this.followers = data.followers;
+          this.following = data.following;
+        });
+    },
   },
   mounted() {
-    this.scope = 'published'
-    this.fetch()
+    this.scope = "published";
+    this.fetch();
   },
   watch: {
-    '$route.params.address': function () {
-      this.fetch()
-    }
-  }
-}
+    "$route.params.address": function() {
+      this.fetch();
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -125,63 +136,64 @@ export default {
   padding: 0;
   margin: 0;
 }
- #title {
+#title {
   display: inline-block;
   font-weight: bold;
   width: 20%;
- }
- #address {
+}
+#address {
   display: inline-block;
   color: grey;
   width: 60%;
- }
- #avatar {
+}
+#avatar {
   height: 138px;
- }
- #signin {
+}
+#signin {
   display: inline-block;
   width: 20%;
- }
- #no-posts {
+}
+#no-posts {
   margin: 30px 30px 0 30px;
- }
- #name input {
+}
+#name input {
   margin-top: 15px;
   text-align: center;
   margin-bottom: 5px;
- }
- #name h3 {
+}
+#name h3 {
   margin-bottom: 0px;
   padding-bottom: 0px;
- }
- #address div {
+}
+#address div {
   margin-bottom: 45px;
   margin-right: 40px;
- }
- #qr {
+}
+#qr {
   margin-top: -25px;
   font-size: 0.85em;
- }
- .selected {
+}
+.selected {
   color: black;
   text-decoration: none;
   cursor: auto;
   font-weight: bold;
- }
- .scope-title {
+}
+.scope-title {
   display: inline-block;
   text-align: center;
   width: 50%;
- }
- #edit {
+}
+#edit {
   margin-top: 20px;
- }
+}
 
 .active {
   text-decoration: none;
   color: black;
 }
-#account, #privateMessage {
+#account,
+#privateMessage {
   margin: 10px;
   margin-top: 40px;
   background-color: green;
