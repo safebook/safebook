6938049593facb3d205430a44140b39a2f0a29dd
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-06-02 13:45:43 +0200

    feat: router history mode

--- src/router.js
+++ src/router.js
@@ -18,7 +18,7 @@ import About from '@/components/about'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'hash',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     // <!-- Presentation
