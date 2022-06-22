5d2d7dab43930cf4aeb31423938243d6d1e4a84d
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-06-22 16:07:24 +0200

    feat: ipfs

--- src/lib/cli.js
+++ src/lib/cli.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 
-if (!module.parent) {
-  console.log("hello world")
+// const safebook = require('./src/lib/safebook.js')
+import safebook from './safebook.js'
+
+// if (!module.parent) {
+// }
+
+console.log(process.argv)
+
+if (process.argv[2] === 'publish') {
+  safebook.publish()
 }
