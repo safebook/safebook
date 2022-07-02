45cdf19e04ce49acd50664ee17377fede000c776
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-02 13:45:53 +0200

    doc: add Safebook Search

--- src/components/presentation/index.vue
+++ src/components/presentation/index.vue
@@ -6,6 +6,7 @@
       <li>Safebook Pages</li>
       <li>Safebook Drive</li>
       <li>Safebook Messenger</li>
+      <li>Safebook Search</li>
       <li><router-link to="names"><a class="text-blue-500">Safebook Names</a></router-link></li>
       <li><router-link to="secret_bonding"><a class="text-blue-500">Secret Bonding</a></router-link></li>
       <li><router-link to="secret_rendez_vous"><a class="text-blue-500">Secret Rendez-vous</a></router-link></li>
