dbe7e35c8558e34414a6241d3a7b962c3833f1d1
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-06 18:40:29 +0200

    doc

--- src/components/presentation/index.vue
+++ src/components/presentation/index.vue
@@ -8,9 +8,13 @@
       <li>Safebook Messenger</li>
       <li>Safebook Search</li>
       <li><router-link to="names"><a class="text-blue-500">Safebook Names</a></router-link></li>
+      <li><router-link to="similar_projects"><a class="text-blue-500">Similar and comparables projects</a></router-link></li>
+    </ul>
+    <h1><center>Cryptographic protocols</center></h1>
+    <ul class="list-disc list-inside">
       <li><router-link to="secret_bonding"><a class="text-blue-500">Secret Bonding</a></router-link></li>
+      <li>How to discover your friends on the network (using phone numbers for exemples) without giving away your friendlist</li>
       <li><router-link to="secret_rendez_vous"><a class="text-blue-500">Secret Rendez-vous</a></router-link></li>
-      <li><router-link to="similar_projects"><a class="text-blue-500">Similar projects</a></router-link></li>
     </ul>
   </div>
 </template>
