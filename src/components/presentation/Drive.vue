26870dfd8b746a865b3e86e3fdb5e3d687d5c0b7
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-06 18:34:49 +0200

    doc

---
+++ src/components/presentation/Drive.vue
@@ -0,0 +1,17 @@
+<template>
+  <H2>See also</h2>
+  <H3>Tarsnap</H3>
+</template>
+
+<script>
+import { H2, H3, A } from '@/components/typography'
+
+export default {
+  components: { H2, H3, A },
+  data () {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
