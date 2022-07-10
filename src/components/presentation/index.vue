bd2081eb34beddeb034572c3e27afbffe659f14b
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-10 14:22:39 +0200

    doc: add post about metadata

--- src/components/presentation/index.vue
+++ src/components/presentation/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="prose">
     <center class="mt-10"><h1>Table of contents</h1></center>
     <ul class="list-disc list-inside">
       <li><router-link to="intro"><a class="text-blue-500">Presentation</a></router-link></li>
@@ -8,10 +8,11 @@
       <li>Safebook Messenger</li>
       <li>Safebook Search</li>
       <li><router-link to="names"><a class="text-blue-500">Safebook Names</a></router-link></li>
-      <li><router-link to="similar_projects"><a class="text-blue-500">Similar and comparables projects</a></router-link></li>
+      <li><router-link to="similar_projects"><a class="text-blue-500">Related projects</a></router-link></li>
     </ul>
     <h1><center>Cryptographic protocols</center></h1>
     <ul class="list-disc list-inside">
+      <li><router-link to="metadata_in_signal">The metadata problem</router-link></li>
       <li><router-link to="secret_bonding"><a class="text-blue-500">Secret Bonding</a></router-link></li>
       <li><router-link to="secret_rendez_vous"><a class="text-blue-500">Secret Rendez-vous</a></router-link></li>
       <li>(Move to previous link)<br />
