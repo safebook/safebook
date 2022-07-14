9323115a44348d3daec044d04257a5f624e1ca35
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-14 19:10:18 +0200

    doc: similar projects: html -> markdown

--- src/components/presentation/SimilarProjects.vue
+++ src/components/presentation/SimilarProjects.vue
@@ -1,69 +1,65 @@
+<script setup>
+const source = `
+## Similar projects
+
+### [Secure Scuttlebutt](https://scuttlebutt.nz)
+
+A gossip peer-to-peer protocol.
+
+Feeds are represented as a hashchain of signed messages.
+
+### [Berty](https://berty.tech)
+
+A truly peer-to-peer, uncensorable and offline first messaging app.<br />
+Based on IPFS
+
+### [Bitmessage](https://bitmessage.org)
+
+
+A radical no-metadata messaging app. Every message is a hidden blob that
+you will try to decipher with all your keys, if you succeed, the message
+is for you. (Scaling is a issue)
+
+TOR-routing of messages is also offered.
+
+### [Lens](https://lens.xyz)
+
+A social network on a blockchain (Polygon).
+
+Personnal consideration : I don't know if a pure on-chain approch could
+scale well, every validator would have to validate every post, comments,
+likes, ...
+
+<small>
+  See : <A href="https://lenster.xyz">lenster.xyz</A>, <A href="https://mirror.xyz">mirror.xyz</A>
+</small>
+
+### [Ceramic](https://ceramic.network)
+
+https://blog.ceramic.network/tiles-a-browser-for-open-source-information/
+
+### [DAT](https://www.datprotocol.com)
+
+### [Retroshare](https://retroshare.cc)
+
+### Freenet, I2P, Zeronet
+
+### [Perfect Dark](https://fr.wikipedia.org/wiki/Perfect_Dark_(P2P))
+
+### Retroshare
+
+## Interesting technologies
+
+### Zero-Knowledge proofs
+
+https://twitter.com/LuozhuZhang/status/1530984087061929984?s=20&t=_6d66haeswHBHNn4cEB46A
+
+https://research.protocol.ai/sites/snarks/
+`
+</script>
+
 <template>
-  <div>
-    <H2>Similar projects</H2>
-    <H3><A href="https://scuttlebutt.nz">Secure Scuttlebutt</A></H3>
-    <p>
-      A gossip peer-to-peer protocol.<br />
-      Feeds are represented as a hashchain of signed messages.
-    </p>
-    <H3><A href="https://berty.tech">Berty</A></H3>
-    <p>
-      A truly peer-to-peer, uncensorable and offline first messaging app.<br />
-      Based on IPFS
-    </p>
-    <H3><A href="https://bitmessage.org">Bitmessage</A></H3>
-    <p>
-      A radical no-metadata messaging app. Every message is a hidden blob that
-      you will try to decipher with all your keys, if you succeed, the message
-      is for you. (Scaling is a issue)<br />
-      TOR-routing of messages is also offered.<br />
-    </p>
-    <H3><A href="https://lens.xyz">Lens</A></H3>
-    <p>A social network on a blockchain (Polygon).</p>
-    <p>
-      Personnal consideration : I don't know if a pure on-chain approch could
-      scale well, every validator would have to validate every post, comments,
-      likes, ...
-    </p>
-    <p>
-      <small>
-        See :
-        <A href="https://lenster.xyz">lenster.xyz</A>
-        ,
-        <A href="https://mirror.xyz">mirror.xyz</A>
-      </small>
-    </p>
-    <H3><A href="https://ceramic.network/">Ceramic</A></H3>
-    <p>
-      https://blog.ceramic.network/tiles-a-browser-for-open-source-information/
-    </p>
-    <H3><A href="https://www.datprotocol.com/">DAT</A></H3>
-    <H3><A href="https://retroshare.cc">Retroshare</A></H3>
-    <p>Freenet, I2P, Zeronet</p>
-    <H2>Other technologies</H2>
-    <H3>Zero-Knowledge proofs</H3>
-    <p>
-      https://twitter.com/LuozhuZhang/status/1530984087061929984?s=20&t=_6d66haeswHBHNn4cEB46A
-      <br />
-      https://research.protocol.ai/sites/snarks/
-    </p>
-    <H3
-      ><A href="https://fr.wikipedia.org/wiki/Perfect_Dark_(P2P)"
-        >Perfect Dark</A
-      ></H3
-    >
-    <H3>Retroshare</H3>
+  <div class="prose lg:prose-xl">
+    <vue3-markdown-it :source="source" html="true" />
   </div>
 </template>
-
-<script>
-import { H2, H3, A } from "@/components/typography/index.js";
-
-export default {
-  components: { H2, H3, A },
-  data() {
-    return {};
-  },
-  methods: {},
-};
-</script>
