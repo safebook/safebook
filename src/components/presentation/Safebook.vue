d8123e63796763d99e2206f30b5e8b178b407683
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-14 17:48:39 +0200

    doc

--- src/components/presentation/Safebook.vue
+++ src/components/presentation/Safebook.vue
@@ -74,6 +74,6 @@ You can follow someone by signing his address. The web-of-followers and web-of-f
 
 <template>
   <div class="prose lg:prose-xl">
-    <vue3-markdown-it :source="source" />
+    <vue3-markdown-it :source="source" html=true />
   </div>
 </template>
