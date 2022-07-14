adeea86c106b5ab3e74a3bdb063dfbed307e7d9c
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-14 18:54:22 +0200

    doc: markdown and more on Berty's metadata

--- src/components/presentation/MetaDataInSignal.vue
+++ src/components/presentation/MetaDataInSignal.vue
@@ -1,37 +1,43 @@
+<script setup>
+const source = `
+## The metadata problem
+
+### Signal
+
+Dans le cas d'une fuite de la base de donnée de Signal, même si le contenu
+des messages est chiffré, on peut tout de même extraire beaucoup
+d'informations.
+
+Tous les nom d'utilisateur et leurs numéros de téléphone.
+
+Leurs amis et donc reconstruire le graphe social entre tous les utilisateurs.
+
+Qui parle à qui et quand, les heures d'activités, etc
+
+Un message dans signal contient un timestamp, l'addresse de l'envoyeur et du destinataire.
+
+[(code source)](https://github.com/signalapp/Signal-Server/blob/main/service/src/main/proto/TextSecure.proto)
+
+### Berty
+
+Les comptes sont anonymes (pas associé à un numéro de téléphone ou à un email)
+
+Lorsqu'on rejoint un groupe, on le fait sous une autre identité, ce qui empeche de connaitre les utilisateurs du groupe.
+
+Les conversation 1-1 sont aussi des implémentés sous forme de groupes, ce qui empeche de connaitre nos correspondants.
+
+Une conversation -> Une identité.
+
+Cepedant, il faut vraiment faire attention car la base de donnée étant décentralisée (distribuée), elle est plus facilement accessible.
+
+### Le champion: Bitmessage
+
+Aucune fuite de métadonnées
+`
+</script>
+
 <template>
-  <div>
-    <h2>Signal</h2>
-    <p>
-      Dans le cas d'une fuite de la base de donnée de Signal, même si le contenu
-      des messages est chiffré, on peut tout de même extraire beaucoup
-      d'informations.
-    </p>
-    <ul class="list-disc list-inside">
-      <li>Tous les nom d'utilisateur et leurs numéros de téléphone.</li>
-      <li>
-        Leurs amis et donc reconstruire le graphe social entre tous les
-        utilisateurs.
-      </li>
-      <li>Qui parle à qui et quand, les heures d'activités, etc</li>
-    </ul>
-    <p>
-      Un message dans signal contient un timestamp, l'addresse de l'envoyeur et
-      du destinataire.
-      <a
-        class="text-blue-500"
-        href="https://github.com/signalapp/Signal-Server/blob/main/service/src/main/proto/TextSecure.proto"
-        >(code source)</a
-      >
-    </p>
-    <h2>Le champion: Bitmessage</h2>
-    <p>Aucune fuite de métadonnées</p>
-    <h2>Berty</h2>
-    <p>Meme problème il me semble.</p>
-    <p>
-      C'est d'autant plus embettant que la base de donnée est accessible de
-      manière casi publique, car décentralisé (architecture distribuée sur
-      IPFS).
-    </p>
-    <h2>Autres</h2>
+  <div class="prose lg:prose-xl">
+    <vue3-markdown-it :source="source" html="true" />
   </div>
 </template>
--- src/components/presentation/SecretBonding.vue
+++ src/components/presentation/SecretBonding.vue
@@ -1,28 +1,24 @@
+<script setup>
+const source = `
+## Summary
+
+We want to be able to "poke" or "like" a person in a serverless setup
+without making the information public.
+
+## Protocol
+
+Step 1: Compute the ECDH bonding with the person that you want to poke or
+like
+
+Step 2: Compute the beacon. <code>Beacon = Hash(ECDH shared key)</code>
+
+Step 3: The person B can now see if it is liked or poked by the person A
+by computing the same beacon and checking if it exist in the network DHT.
+`
+</script>
+
 <template>
-  <div>
-    <H2>Summary</H2>
-    <p>
-      We want to be able to "poke" or "like" a person in a serverless setup
-      without making the information public.
-    </p>
-    <H2>Protocol</H2>
-    <p>
-      Step 1: Compute the ECDH bonding with the person that you want to poke or
-      like
-    </p>
-    <p>
-      Step 2: Compute the beacon. <code>Beacon = Hash(ECDH shared key)</code>
-    </p>
-    <p>
-      Step 3: The person B can now see if it is liked or poked by the person A
-      by computing the same beacon and checking if it exist in the network DHT.
-    </p>
+  <div class="prose lg:prose-xl">
+    <vue3-markdown-it :source="source" html="true" />
   </div>
 </template>
-
-<script>
-import { H2 } from "@/components/typography/index.js";
-export default {
-  components: { H2 },
-};
-</script>
