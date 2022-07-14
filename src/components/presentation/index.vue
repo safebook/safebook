b9afc22394c8ec05d4b8c4fd55c14b9debc5ef3b
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-14 19:29:59 +0200

    doc: rework index

--- src/components/presentation/index.vue
+++ src/components/presentation/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="prose">
-    <center class="mt-10"><h1>Table of contents</h1></center>
+    <center class="mt-10"><h1>Subprojects</h1></center>
     <ul class="list-disc list-inside">
       <li>
         <router-link to="intro"
@@ -30,6 +30,7 @@
           to offline search, and with an additionnal YaCy backend</small
         >
       </li>
+      <li><small>Safebook Password Manager. A state of the art determinist and non-deterministic password manager</small></li>
       <li>
         <router-link to="similar_projects"
           ><a class="text-blue-500">Related projects</a></router-link
@@ -39,7 +40,7 @@
     <h1><center>Cryptographic protocols</center></h1>
     <ul class="list-disc list-inside">
       <li>
-        <router-link to="metadata_in_signal">The metadata problem</router-link>
+        <router-link to="metadata_in_signal"><a class="text-blue-500">The metadata problem</a></router-link>
       </li>
       <li>
         <router-link to="secret_bonding"
@@ -50,29 +51,6 @@
         <router-link to="secret_rendez_vous"
           ><a class="text-blue-500">Secret Rendez-vous</a></router-link
         >
-      </li>
-      <li>
-        (Move to previous link)<br />
-        To prevent RDV reuse we can derive n RDVs from one (kept secret) RDV
-        seed using H(RDV + "0"), H(RDV + "1"), ...<br />
-        It could also be powerful to use BIP32's CKDpub to compute child public
-        keys from the parent public key withou access to private keys, for a
-        semi-trusted setup
-      </li>
-      <li>
-        Rendez-vous point pre-computation advantages for a social dark web.<br />
-        Solution for the metadata problem on a messaging app<br />
-        Let's say there is 10B users and we can precompute 30M per second. less
-        that 1h and less than 10GB are necessary to precompute all
-        associations.<br />
-        Given that we have a secret rendez-vous point with every users, we can
-        listen to everyone, but we shouldn't advertise what are the secret
-        rendez-vous point that we listen, because it will obviously give away
-        that we are one of the parcticipant (sender or receiver).<br />
-        While it should be possible for the network to store every messages for
-        a sufficient period of time using a DHT,<br />
-        It would be impossible for a single user to process all messages. That
-        is where bloom filters takes place.
       </li>
       <li>
         How to discover your friends on the network (using phone numbers for
@@ -92,17 +70,21 @@
     <h3>Misc</h3>
     <ul class="list-disc list-inside">
       <li>
-        Hash payement. Where customers pays but the company makes no money.
-        Could be a PoW difficulty target for different hash difficulty
-        (cpu,gpu,memory,proof of space-time?) or even useful work like folding
-        proteins or finding primes. The organisator or DOA sets the difficulty
-        target. Could be used for - Requesting ressource - Sending message (ex:
-        Bitmessage) - DDNS name registration - Tokenless blockchain
+        <small>
+          Hash payement. Where customers pays but the company makes no money.
+          Could be a PoW difficulty target for different hash difficulty
+          (cpu,gpu,memory,proof of space-time?) or even useful work like folding
+          proteins or finding primes. The organisator or DOA sets the difficulty
+          target. Could be used for - Requesting ressource - Sending message (ex:
+          Bitmessage) - DDNS name registration - Tokenless blockchain
+        </small>
       </li>
       <li>
-        A company or system for quickly building a web-of-trust. for exemple
-        associating a pubkey to a phone number. You could get tokens or money to
-        verify someone's else number
+        <small>
+          A company or system for quickly building a web-of-trust. for exemple
+          associating a pubkey to a phone number. You could get tokens or money to
+          verify someone's else number
+        </small>
       </li>
     </ul>
   </div>
