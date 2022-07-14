e8e804be631d7dc3a50fd3e0603f28800a5db297
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-07-14 19:29:34 +0200

    doc: secret rendez-vous

--- src/components/presentation/SecretRendezVous.vue
+++ src/components/presentation/SecretRendezVous.vue
@@ -1,5 +1,14 @@
 <script setup>
 const source = `
+## Secret rendez-vous
+
+### Table of Contents
+1. [Contexte](#context)
+2. [Points de rendez-vous](#rendez-vous-points)
+3. [The necessity of precomputation](#precomputation)
+4. [Anonymity filters](#anonymity-filters)
+5. Extra: Rendez-vous tree
+
 ## Contexte
 
 Un logiciel peer-to-peer, utilisant par exemple une DHT comme couche de
@@ -25,12 +34,33 @@ Pour plus de détails au sujet des métadonnées vous pouvez lire [une critique 
 
 Nous voulons rendre inexploitable les métadonnées.
 
+## Precomputation
+
+Rendez-vous point pre-computation advantages for a social dark web.
+
+Solution for the metadata problem on a messaging app
+
+Let's say there is 10B users and we can precompute 30M per second. less
+that 1h and less than 10GB are necessary to precompute all
+associations.
+
+Given that we have a secret rendez-vous point with every users, we can
+listen to everyone, but we shouldn't advertise what are the secret
+rendez-vous point that we listen, because it will obviously give away
+that we are one of the parcticipant (sender or receiver).
+
+While it should be possible for the network to store every messages for
+a sufficient period of time using a DHT,
+
+It would be impossible for a single user to process all messages. That
+is where bloom filters takes place.
+
 ## Filtres de bloom
 
 Un filtre de bloom est une structure de donnée probabilistique permettant
 de savoir rapidement si un élément appartient à un ensemble.
 
-## Utilisation pour l'anonymité
+### Utilisation pour l'anonymité
 
 Lorsqu'on utilise un filtre de Bloom, on ne dévoile pas quels éléments on
 recherche.
@@ -40,11 +70,21 @@ quelquechose (des messages, etc) à un pair.
 
 Ainsi, on evite de fuiter des métadonnées sur quels sont nos contacts, etc
 
-## Exemple de construction du filtre pour un ensemble de points de rendez-vous
+### Exemple de construction du filtre pour un ensemble de points de rendez-vous
 
 Pour simplifier on utilise une seule fonction de hash H
 
 On rajoute un sel en redefinissant la fonction de hash comme <code>H'(x) = H(salt + x)</code>
+
+## Extra: RDV tree for almost infinite "secret key" RDV point generation without knowing the secret key
+
+*Beware, here rendez-vous points refer to a public key/secret keys couple*
+
+To prevent RDV reuse we can derive n RDVs from one (kept secret) RDV
+seed using H(RDV + "0"), H(RDV + "1"), ...<br />
+It could also be powerful to use BIP32's CKDpub to compute child public
+keys from the parent public key withou access to private keys, for a
+semi-trusted setup
 `
 </script>
 
