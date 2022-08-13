44a86b00c8144c78c0073426136ed4809e224ce8
Maxime Lalisse <maxime.lalisse@gmail.com>
2022-08-13 15:18:15 +0200

    cleanup

--- src/components/about/index.vue
+++ src/components/about/index.vue
@@ -3,51 +3,7 @@
 </template>
 
 <script>
-const source = `
-# About
-
-# Similar tools
-
-- Berty
-- Secure Scuttlebutt
-- Status
-- Signal
-
-## History
-
-Safebook is a personal project that started as a browser plugin encrypting
-facebook messages before they are sent and decrypting safebook messages when
-found
-
-Because facebook is listening to every keystroke (for suggestions, etc) it is
-not possible to hide messages this way so it has been abandoned in profit of
-a separate website.
-
-It was abandonned for 5+ years before this new attemps.
-New ideas were coined like base58 encoding for address
-with SB prefix vanity addresses, shadow addresses, signing for publication,
-and now aim to be server independant to allow different server
-implementations (and hopefully a decentralised one)
-
-## Other tools
-
-### PGP/GPG
-
-PGP is the reference.
-It's quite old but it provides state-of-the-art cryptographic. difficult to use
-
-### Minilock
-
-### Bitmessage
-
-## Crypto libraries
-
-sjcl
-nacl
-celle de l'auteur de real world crypto
-openssl
-...
-`;
+const source = ``;
 
 export default {
   name: "About",
--- src/components/presentation/index.vue
+++ src/components/presentation/index.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="prose">
-    <h3>Decentralizing signal</h3>
-    <h3>The safebook project</h3>
-    <h3>Cryptographic protocols</h3>
-
-    <center class="mt-10"><h1>Legacy</h1></center>
-
     <center class="mt-10"><h1>Subprojects</h1></center>
     <ul class="list-disc list-inside">
       <li><router-link to="intro"><a class="text-blue-500">Presentation</a></router-link></li>
@@ -15,8 +9,8 @@
       <li><router-link to="names"><a class="text-blue-500">Safebook Names</a></router-link></li>
       <li>
         <small
-          >Safebook Search (based on aggressive adveserial caching of every search from the search provider, and p2p sharing of those results, default
-          to offline search, and with an additionnal YaCy backend</small
+          >Safebook Search: Yacy + advesarial caching of every search from the search provide
+          + indexing facility + default to offline search, and with an additionnal YaCy backend</small
         >
       </li>
       <li><small>Safebook Password Manager. A state of the art determinist and non-deterministic password manager. <b>Best might be just generating the seed for an open source pw manager</b></small></li>
@@ -35,57 +29,6 @@
         <router-link to="secret_rendez_vous"
           ><a class="text-blue-500">Secret Rendez-vous</a></router-link
         >
-      </li>
-      <li>
-        How to discover your friends on the network (using phone numbers for
-        exemples) without giving away your friendlist.<br />
-        Part1: Associate your phone number, email address or other
-        identification mean with a public key. This could be done using
-        web-of-trust. (This is a topic on its own)<br />
-        Part2: Compute a kind of "secret rendez-vous point" (H(K_a^k_b) ==
-        H(K_b^k_a)) from your public key (eg: associated with your phone number)
-        with all your friends "phone-numbers" public key and publish it<br />
-        Part3: Finding a published token linking our phone number to someone's
-        else means that our this is indeed our friend's public key<br />
-        [It is true indeed that after Part1, we don't need Part2 and Part3
-        because we already know our friends public keys]
-      </li>
-    </ul>
-    <h3>Misc</h3>
-    <ul class="list-disc list-inside">
-      <li>
-        <small>
-          Hash payement. Where customers pays but the company makes no money.
-          Could be a PoW difficulty target for different hash difficulty
-          (cpu,gpu,memory,proof of space-time?) or even useful work like folding
-          proteins or finding primes. The organisator or DOA sets the difficulty
-          target. Could be used for - Requesting ressource - Sending message (ex:
-          Bitmessage) - DDNS name registration - Tokenless blockchain
-        </small>
-      </li>
-      <li>
-        <small>
-          A company or system for quickly building a web-of-trust. for exemple
-          associating a pubkey to a phone number. You could get tokens or money to
-          verify someone's else number
-        </small>
-        <p>Ex: WoT for associating a pubkey to a phone number.</p>
-      </li>
-      <li>
-        Safebook started as a browser plugin for encrypting
-        facebook messages before they are sent and decrypting safebook messages when
-        found.
-        <br />
-        Quite similary of what mask.io do now.
-        <br />
-        Because facebook is listening to every keystroke (for bad and good reasons (ex: suggestions etc)) it is
-        not possible to inject a script in the page that hide messages after they have been typed without leaking informations, so the on-facebook encyption (and decryption) has been abandoned in profit of encryptiong on a separate page.
-        <br />
-        It was abandonned for before this new attemps, in a totally different direction.
-        <br />
-        New ideas are borrowed from other projects like bitcoin, bitmessages. etc.
-        Ex:  base58 encoding for address with SB prefix vanity addresses, shadow addresses, signing for publication,
-        and now aim to be server independant to allow different server implementations (and hopefully a decentralised one)
       </li>
     </ul>
   </div>
